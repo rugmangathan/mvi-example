9d61a0d78e26a2dae3ac888c372685fcf9d56dd2
Rugmangathan <mkrugmangathan@gmail.com>
2017-12-10 15:20:02 +0530

    Budapest, created MVI files.

---
+++ OttraiThisaiOttam/Budapest/BudapestModel.swift
@@ -0,0 +1,11 @@
+//
+// Copyright © 2017 Rugmangathan. All rights reserved.
+//
+// Unauthorized copying of this file, via any medium is strictly prohibited.
+// Proprietary and confidential.
+//
+
+import Foundation
+
+class BudapestModel {
+}
---
+++ OttraiThisaiOttam/Budapest/BudapestViewController.swift
@@ -0,0 +1,14 @@
+//
+// Copyright © 2017 Rugmangathan. All rights reserved.
+//
+// Unauthorized copying of this file, via any medium is strictly prohibited.
+// Proprietary and confidential.
+//
+
+import UIKit
+
+class BudapestViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
