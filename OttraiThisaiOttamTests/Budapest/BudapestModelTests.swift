6f248cad11ff1d11031ae1dce1d35fdc19183159
Rugmangathan <mkrugmangathan@gmail.com>
2017-12-10 15:22:17 +0530

    BudapestModelTests, created.

---
+++ OttraiThisaiOttamTests/Budapest/BudapestModelTests.swift
@@ -0,0 +1,12 @@
+//
+//  BudapestModelTests.swift
+//  OttraiThisaiOttamTests
+//
+//  Created by Rugmangathan on 10/12/17.
+//  Copyright © 2017 Rugmangathan. All rights reserved.
+//
+
+import XCTest
+
+class BudapestModelTests: XCTestCase {    
+}
