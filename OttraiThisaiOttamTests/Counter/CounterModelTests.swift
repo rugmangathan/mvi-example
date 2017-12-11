87075d5a1287244f18e4a79c08f9fcfedfdfa68c
Rugmangathan <mkrugmangathan@gmail.com>
2017-12-11 18:02:59 +0530

    CounterModelTests, created.

---
+++ OttraiThisaiOttamTests/Counter/CounterModelTests.swift
@@ -0,0 +1,13 @@
+//
+//  CounterModelTests.swift
+//  OttraiThisaiOttamTests
+//
+//  Created by Rugmangathan on 11/12/17.
+//  Copyright © 2017 Rugmangathan. All rights reserved.
+//
+
+import XCTest
+@testable import OttraiThisaiOttam
+
+class CounterModelTests: XCTestCase {
+}
