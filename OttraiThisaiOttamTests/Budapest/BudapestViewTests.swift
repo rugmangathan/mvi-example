921a248307c400444653545284f8fac4dbab5224
Rugmangathan <mkrugmangathan@gmail.com>
2017-12-11 14:26:12 +0530

    SpyableBudapestView, created.

---
+++ OttraiThisaiOttamTests/Budapest/BudapestViewTests.swift
@@ -0,0 +1,15 @@
+//
+//  BudapestViewTests.swift
+//  OttraiThisaiOttamTests
+//
+//  Created by Rugmangathan on 11/12/17.
+//  Copyright © 2017 Rugmangathan. All rights reserved.
+//
+
+import XCTest
+import Cuckoo
+@testable import OttraiThisaiOttam
+
+class BudapestViewTests: XCTestCase {
+  
+}
---
+++ OttraiThisaiOttamTests/Budapest/SpyableBudapestView.swift
@@ -0,0 +1,13 @@
+//
+//  SpyableBudapestView.swift
+//  OttraiThisaiOttamTests
+//
+//  Created by Rugmangathan on 11/12/17.
+//  Copyright © 2017 Rugmangathan. All rights reserved.
+//
+
+import Foundation
+@testable import OttraiThisaiOttam
+
+class SpyableBudapestView: BudapestView {
+}
